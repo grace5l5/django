ec39d083446a23ad43b05d65a5a3d31ebdc020b4
Grace5l5 <gs.leungshing@gmail.com>
2018-10-12 12:57:18 +0400

    Cleaned up homework, still needs to fix link to open in new tab for github links

--- urls.py
+++ urls.py
@@ -5,11 +5,9 @@ import views
 # In this example, we've separated out the views.py into a new file
 urlpatterns = [
     path('', views.index),
-    path('about-me', views.about_me),
     path('contact-me', views.contact_me),
     path('blogs', views.blogs),
     path('projects', views.projects),
-    path('github-api-example', views.github_api_example),
 ]
 
 # Boilerplate to include static files
--- views.py
+++ views.py
@@ -3,11 +3,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    content_html = open("content/index.html").read()
-    context = {
-        "content": content_html
-    }
-    return render(request, "base.html", context)
+    context = {}
+    return render(request, "index.html", context)
 
 
 def about_me(request):
@@ -22,35 +19,21 @@ def about_me(request):
 
 
 def contact_me(request):
-    content_html = open("content/contact.html").read()
-    context = {
-        "content": content_html
-    }
-    return render(request, "base.html", context)
+    context = {}
+    return render(request, "contact.html", context)
 
 
 def blogs(request):
-    content_html = open("content/blogs.html").read()
-    context = {
-        "content": content_html
-    }
-    return render(request, "base.html", context)
+    context = {}
+    return render(request, "blogs.html", context)
 
 
 def projects(request):
-    content_html = open("content/projects.html").read()
-    context = {
-        "content": content_html,
-    }
-    return render(request, "base.html", context)
-
-
-def github_api_example(request):
-    # We can also combine Django with APIs
     response = requests.get('https://api.github.com/users/grace5l5/repos')
     repos = response.json()
     context = {
-        'github_repos': repos,
+        'github_repos': repos,    
     }
-    return render(request, "github.html", context)
+    return render(request, "projects.html", context)
+
 
